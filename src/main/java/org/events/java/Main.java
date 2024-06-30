6f9540e0975f93df0702e4e17f084e169bfa79b6
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-06-30 12:02:43 +0200

    testing for Evento class

--- src/main/java/org/events/java/Main.java
+++ src/main/java/org/events/java/Main.java
@@ -5,6 +5,26 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Evento evento = new Evento("concerto", 10, 2025, 12, 7);
+
+		evento.prenota(5);
+
+		System.out.println(evento.getBookedSeats());
+
+		evento.prenota(5);
+
+		System.out.println(evento.getBookedSeats());
+
+		evento.disdici(3);
+
+		System.out.println(evento.getBookedSeats());
+
+		evento.prenota(2);
+
+		System.out.println(evento.getBookedSeats());
+
+		evento.prenota(2);
+
 	}
 
 }
