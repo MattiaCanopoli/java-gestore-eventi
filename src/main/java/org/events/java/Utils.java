ce5a02d22f5f0ea6c2b806a6e9f8198834bc64c0
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-07-01 09:33:32 +0200

    add: java docs

--- src/main/java/org/events/java/Utils.java
+++ src/main/java/org/events/java/Utils.java
@@ -2,11 +2,12 @@ package org.events.java;
 
 import java.time.LocalDate;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Utils {
 
 	/**
-	 * Generate a random int
+	 * Generate a random int within limits
 	 * 
 	 * @param max int, upper limit
 	 * @param min int, lower limit
@@ -19,6 +20,13 @@ public class Utils {
 		return randomNumber;
 	}
 
+	/**
+	 * checks if a date is in the future.<br>
+	 * if the input date is the current day or before, return false
+	 * 
+	 * @param inputDate
+	 * @return boolean
+	 */
 	public static boolean checkDate(LocalDate inputDate) {
 		if (inputDate.isBefore(LocalDate.now().plusDays(1))) {
 			return false;
@@ -27,6 +35,18 @@ public class Utils {
 		}
 	}
 
+	/**
+	 * checks if a date is in the future.<br>
+	 * accept a String as argument.the inputDate String must follow the pattern
+	 * <strong>"yyyy-MM-dd"</strong><br>
+	 * dashes within date elements in string are mandatory<br>
+	 * inputDate String is parsed with LocalDate type and then checked<br>
+	 * if it's before or equal to current day, return false
+	 * 
+	 * @param inputDate String with date to check. must follow the pattern
+	 *                  "yyyy-MM-dd"
+	 * @return boolean
+	 */
 	public static boolean checkDate(String inputDate) {
 		LocalDate date = LocalDate.parse(inputDate);
 
@@ -35,6 +55,39 @@ public class Utils {
 		} else {
 			return true;
 		}
+	}
+
+	/**
+	 * ask question to user and checks if answer is an integer.<br>
+	 * if user provides an integer, return the integer given.<br>
+	 * else, prints invalidImput message and keeps on asking until a valid input is
+	 * provided.<br>
+	 * 
+	 * @param scanner             a scanner instance
+	 * @param question            a string representing the question to ask
+	 * @param invalidImputMessage a string representing the message to print in case
+	 *                            of invalid input
+	 * @return integer asked
+	 */
+	public static int checkIntImput(Scanner scanner, String question, String invalidImputMessage) {
+		boolean check = false;
+		int number = 0;
+
+		do {
+			System.out.println(question);
+
+			if (scanner.hasNextInt()) {
+				number = scanner.nextInt();
+				scanner.nextLine();
+				check = true;
+			} else {
+				scanner.nextLine();
+				System.out.println(invalidImputMessage);
+			}
+
+		} while (!check);
+
+		return number;
 	}
 
 }
