67cf4de882a2b930f284bb266594261fabb71ed2
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-07-24 06:59:46 +0200

    Create ProgrammaEventi.java
    added constructor

---
+++ src/main/java/org/events/java/ProgrammaEventi.java
@@ -0,0 +1,24 @@
+package org.events.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProgrammaEventi {
+
+	private String programTitle;
+	private List<Evento> eventList;
+
+	/**
+	 * Constructor for a new ProgrammaEventi.<br>
+	 * programTitle is passed as parameter.<br>
+	 * A new empty ArrayList is declared on construction.
+	 * 
+	 * @param programTitle String. title of the ProgrammaEventi
+	 */
+	public ProgrammaEventi(String programTitle) {
+		this.programTitle = programTitle;
+		this.eventList = new ArrayList<Evento>();
+
+	}
+
+}
